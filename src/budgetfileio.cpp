9276495baf63108e7c8dd5d851b74a886030b3bd
Yinuo Peng <yinuop2@uci.edu>
2024-10-12 05:00:48 +0000

    finish budegfileio.cpp

--- src/budgetfileio.cpp
+++ src/budgetfileio.cpp
@@ -1,14 +1,23 @@
 // implement budgetfileio.cpp functions here.
+
 #include <iostream>
 #include <fstream>
 #include "../include/budgetfileio.hpp"
 #include <string>
+#include <sstream>
+#include <iomanip>
 using namespace std;
 
-ifstream inFile;
-ofstream outFile;
+double grossIncome = 0.0;
+double totalExpenses = 0.0;
+double rent = 0.0;
+double utilities = 0.0;
+double groceries = 0.0;
+double entertainment = 0.0;
+double transportation = 0.0;
+double array1[6] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
 
-std::string readExpensesCSV(const std::string& inputFilename,
+string readExpensesCSV(const string& inputFilename,
     double& grossIncome,
     double& totalExpenses,
     double& rent,
@@ -17,17 +26,50 @@ std::string readExpensesCSV(const std::string& inputFilename,
     double& entertainment,
     double& transportation)
     {
-    ifstream file(inputFilename);
-    if (!file.is_open()){
-        return "Error: Cannot open file $OUTPUTFILENAME"
+    ifstream inFile;
+    inFile.open(inputFilename);
+    if (!inFile.is_open()){
+        return "Error: Cannot open file "+inputFilename;
     }
 
     string line;
-    getline(file, line);
-    
+    while (getline(inFile, line)){
+        stringstream currentline(line);
+        string category, type, amount_str, date;
+
+        getline(currentline, category, ',');
+        getline(currentline, type, ',');
+        getline(currentline, amount_str, ',');
+        getline(currentline, date, ',');
+
+        if (category == "Rent"){
+            array1[0] = stod(amount_str);
+        }else if (category == "Utilities"){
+            array1[1] = stod(amount_str);
+        }else if (category == "Groceries"){
+            array1[2] = stod(amount_str);
+        }else if (category == "Entertainment"){
+            array1[3] = stod(amount_str);
+        }else if (category == "Transportation"){
+            array1[4] = stod(amount_str);
+        }else {
+            array1[5] = stod(amount_str);
+        }
+
+        if (type == "Expense"){
+            totalExpenses += stod(amount_str);
+        } else if (type == "Income"){
+            grossIncome += stod(amount_str);
+        }
+    }
+
+    inFile.close();
+
+    return "";
 }
 
-std::string writeBudgetReport(
+
+string writeBudgetReport(
     const std::string& outFilename,
     double grossIncome,
     double totalExpenses,
@@ -37,13 +79,39 @@ std::string writeBudgetReport(
     double entertainment,
     double transportation
 ) {
+    ofstream outFile;
+    outFile.open(outFilename);
+    if (!outFile.is_open()){
+        return "Error: Cannot open file "+outFilename;
+    }
+    double rentPercentage = (rent / grossIncome) * 100;
+    double utilitiesPercentage = (utilities / grossIncome) * 100;
+    double groceriesPercentage = (groceries / grossIncome) * 100;
+    double entertainmentPercentage = (entertainment / grossIncome) * 100;
+    double transportationPercentage = (transportation / grossIncome) * 100;
+    double incomePercentage = (totalExpenses / grossIncome) * 100;
+    double netChange = grossIncome - totalExpenses;
 
-}
+    outFile << fixed << setprecision(2);
 
+    outFile << "+---------------------------------------------+\n";
+    outFile << "|             Budget Analysis Report          |\n";
+    outFile << "+---------------------------------------------+\n";
+    outFile << "| Expenses        | Total Spent | %of Income  |\n";
+    outFile << "+---------------------------------------------+\n";
+    outFile << "| Rent            | $" << setw(10) << rent << " | " << setw(10) << right << rentPercentage << "% |\n";
+    outFile << "| Utilities       | $" << setw(10) << utilities << " | " << setw(10) << right << utilitiesPercentage << "% |\n";
+    outFile << "| Groceries       | $" << setw(10) << groceries << " | " << setw(10) << right << groceriesPercentage << "% |\n";
+    outFile << "| Entertainment   | $" << setw(10) << entertainment << " | " << setw(10) << right << entertainmentPercentage << "% |\n";
+    outFile << "| Transportation  | $" << setw(10) << transportation << " | " << setw(10) << right << transportationPercentage << "% |\n";
+    outFile << "+---------------------------------------------+\n";
 
-int main() {
-    string inputFilename;
-    cout << "Please enter the path of CSV file:"
-    cin >> inputFilename;
+    outFile << "Gross Income: $" << grossIncome << "\n";
+    outFile << "Total Expenses: $" << totalExpenses << "\n";
+    outFile << "Net Change: $" << netChange << "\n";
+    outFile << "Income Percentage: " << incomePercentage << "%\n";
 
+    outFile.close();
+
+    return "";
 }

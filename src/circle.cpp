595ed0845d4b4a688db7fcaefd72e75db0679909
Yinuo Peng <yinuop2@uci.edu>
2024-10-12 00:41:28 +0000

    complete three circle function

--- src/circle.cpp
+++ src/circle.cpp
@@ -1,1 +1,23 @@
 // implement circle.cpp functions here.
+#include "../include/circle.h"
+#include <iostream>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+using namespace std;
+
+double radius(double x1, double y1, double x2, double y2) {
+    double d;
+    d = sqrt(pow((x2-x1), 2)+pow((y2-y1), 2));
+    return d;
+}
+
+double circumference(double rad) {
+    double cir = rad * M_PI;
+    return cir;
+}
+
+double area(double rad) {
+    double area1 = M_PI * pow(rad, 2);
+    return area1;
+}
